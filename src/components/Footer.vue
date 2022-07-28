04e5e245a46464bc965e46bd284065a9838ff394
“kangyu” <kangyu@huatu.com>
2022-07-28 17:17:08 +0800

    feat: add footer components

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer class="foot">
+        <span>Copyright©华图教育-武汉研发中心</span>
+    </footer>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<style>
+.foot {
+    height: 30px;
+    font-size: 12px;
+    line-height: 30px;
+    margin-left: 180px;
+    text-align: center;
+    flex: 0 0 auto;
+    color: #999999
+}
+</style>
