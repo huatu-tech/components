3a0bf5f16028c1dc97f4eadef1812f20e053d071
“kangyu” <kangyu@huatu.com>
2022-07-28 17:18:44 +0800

    feat: add coms

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 状态组件
+let Footer = () => import("./components/Footer.vue");
+const components = [Footer];
+// 使用组件
+const install = function (Vue, opts = {}) {
+  components.forEach((component) => {
+    Vue.component(component.name, component);
+  });
+};
+/* istanbul ignore if */
+if (typeof window !== "undefined" && window.Vue) {
+  install(window.Vue);
+}
+// 也可以按需引入
+export default {
+  version: "1.0.6",
+  Footer,
+  install,
+};
 
 Vue.config.productionTip = false
 
