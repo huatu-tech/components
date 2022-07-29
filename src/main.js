79cdf66d7f096a2ab952b63111882665c7a4d9d3
“kangyu” <kangyu@huatu.com>
2022-07-29 14:44:23 +0800

    feat: add components

--- src/main.js
+++ src/main.js
@@ -1,17 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 // 状态组件
-import Footer from '../packages/footer/index.js';
-const components = [Footer];
+import Footer from '../packages/footer';
+import LoadingBar from '../packages/loading-bar';
+import ApproveFlow from '../packages/approve-flow';
+import BackTop from '../packages/back-top';
+
+
+const components = [Footer, ApproveFlow, BackTop];
+
 // 使用组件
 const install = function (Vue, opts = {}) {
+
   components.forEach((component) => {
     Vue.component(component.name, component);
   });
+
   Vue.prototype.$ELEMENT = {
     size: opts.size || '',
     zIndex: opts.zIndex || 2000
   };
+
+
+  Vue.prototype.$loadingBar = LoadingBar
+
 };
 /* istanbul ignore if */
 if (typeof window !== "undefined" && window.Vue) {
@@ -24,8 +36,8 @@ export default {
   Footer,
 };
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
