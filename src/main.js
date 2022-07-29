3727224f3270421a71c94a47250fc3bd0c8d928a
“kangyu” <kangyu@huatu.com>
2022-07-29 11:53:05 +0800

    feat: change directory structure

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 // 状态组件
-let Footer = () => import("./components/Footer.vue");
+import Footer from '../packages/footer/index.js';
 const components = [Footer];
 // 使用组件
 const install = function (Vue, opts = {}) {
   components.forEach((component) => {
     Vue.component(component.name, component);
   });
+  Vue.prototype.$ELEMENT = {
+    size: opts.size || '',
+    zIndex: opts.zIndex || 2000
+  };
 };
 /* istanbul ignore if */
 if (typeof window !== "undefined" && window.Vue) {
@@ -16,8 +20,8 @@ if (typeof window !== "undefined" && window.Vue) {
 // 也可以按需引入
 export default {
   version: "1.0.6",
-  Footer,
   install,
+  Footer,
 };
 
 Vue.config.productionTip = false
